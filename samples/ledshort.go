d2351f568655f93ae8bd7a55e052774d206e1960
Suvir Kumar <suvir@talkkonnect.com>
2021-08-24 19:33:03 +0700

    fork embd

---
+++ samples/ledshort.go
@@ -0,0 +1,25 @@
+// +build ignore
+
+// Short LED example, works OOTB on a BBB.
+
+package main
+
+import (
+	"flag"
+	"time"
+
+	"github.com/talkkonnect/embd"
+
+	_ "github.com/talkkonnect/embd/host/bbb"
+)
+
+func main() {
+	flag.Parse()
+
+	embd.InitLED()
+	defer embd.CloseLED()
+
+	embd.LEDOn(3)
+	time.Sleep(1 * time.Second)
+	embd.LEDOff(3)
+}
